7778cb2bdaada44898ef156177c89105fd958af4
Farkas <fcsabi789@gmail.com>
2022-04-28 09:13:50 +0200

    lekérés müködik

--- src/index.php
+++ src/index.php
@@ -6,5 +6,29 @@ if($con->connect_error){
     die("Hiba! A kapcsolodás sikertelen.");
 }
 
-echo "ok";
+//echo "ok";
+
+
+$sql=<<<EOT
+
+select * from dolgozok
+
+
+EOT;
+if($result=$con->query($sql)){
+echo "Lekérés OK";
+}else{
+    echo "A lekérés Sikertelen";
+}
+
+
+
+while($row=$result->fetch_assoc()){
+echo $row['nev'];
+}
+
+
+
+
+
 $con->close();

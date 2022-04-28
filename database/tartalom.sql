b576048c2741120e543e2417db17bdd28baaa4c4
Farkas <fcsabi789@gmail.com>
2022-04-28 08:50:50 +0200

    Adatbázisba nevek

---
+++ database/tartalom.sql
@@ -0,0 +1,9 @@
+insert into dolgozok 
+(nev, település, fizetes)
+values
+('Tango Béla','Szeged',457),
+('Szilágyi Áron','Csömör',4667),
+('Rózsa Gergő','Pécel',43),
+('Grosics Gyula','Szeged',87),
+('Kiss Béla','Szolnok',99957),
+('Farkas Csaba','Budapest',10234);
